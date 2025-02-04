de6cb71d8790198167ab98f69de4da776ad7aab2
Mathew Terhune <mathewterhune@gmail.com>
2025-02-03 19:51:07 -0800

    Updated: onMouseDown and on MouseEnter

--- src/components/node.jsx
+++ src/components/node.jsx
@@ -1,22 +1,22 @@
-const Node = ({ size, state, onClick }) => {
-    // Function to determine the background color based on state
-    const getColor = () => {
-      if (state === "wall") return "bg-black"; // Walls
-      if (state === "selected") return "bg-red-500"; // Selected
-      return "bg-green-500"; // Default
-    };
-  
-    return (
-      <div
-        className={`border border-gray-400 ${getColor()}`}
-        style={{
-          width: `${size}px`,
-          height: `${size}px`,
-        }}
-        onClick={onClick} // Clicking updates state in Grid
-      ></div>
-    );
+const Node = ({ size, state, onMouseDown, onMouseEnter }) => {
+  // Function to determine the background color based on state
+  const getColor = () => {
+    if (state === "wall") return "bg-black"; // Walls
+    if (state === "selected") return "bg-red-500"; // Selected
+    return "bg-green-500"; // Default
   };
-  
-  export default Node;
-  
+
+  return (
+    <div
+      className={`border border-gray-400 ${getColor()}`}
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+      }}
+      onMouseDown={onMouseDown} // Start wall-drawing
+      onMouseEnter={onMouseEnter} // Drag to draw walls
+    ></div>
+  );
+};
+
+export default Node;
